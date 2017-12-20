cf7925ac640907924cb92b7595d8e8679d43d6fc
Fitz <f18846188605@gmail.com>
2017-12-20 23:51:55 +0800

    start pc module

---
+++ myY-86.srcs/sources_1/new/intd_fetch.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2017/12/20 23:13:09
+// Design Name: 
+// Module Name: intd_fetch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module intd_fetch(
+
+    );
+endmodule
