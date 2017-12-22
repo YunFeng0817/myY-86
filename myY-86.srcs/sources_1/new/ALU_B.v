04314a5d3f418e56b9ba7cc01be60a4573f4006a
Fitz <f18846188605@gmail.com>
2017-12-22 22:37:23 +0800

    complete the ALU choose B module

---
+++ myY-86.srcs/sources_1/new/ALU_B.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+`include "const_defination.v"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2017/12/22 22:03:39
+// Design Name: 
+// Module Name: ALU_B
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_B(
+    input wire[`icodeBus] icode,
+    input wire[`ifunBus] ifun,
+    input wire[`digitsBus] valB,
+    output reg[`digitsBus] ALUB
+    );
+    
+    always@(*)
+    begin
+        case({icode,4'h0})
+            `rrmovq:
+                begin
+                    ALUB<=0;
+                end
+            {`rmmovq,`mrmovq,`addq,`ret,`popq,`pushq,`call}:
+                begin
+                    ALUB<=valB;
+                end
+            `irmovq:
+                begin
+                    if(ifun==4'h0)
+                    begin
+                        ALUB<=0;
+                    end
+                    else
+                    begin
+                        ALUB<=valB;
+                    end
+                end
+        endcase
+    end
+    
+endmodule
