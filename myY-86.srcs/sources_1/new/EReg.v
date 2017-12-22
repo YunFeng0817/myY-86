c236350eb14c0f74bc79c6ceff905f568f58fe21
Fitz <f18846188605@gmail.com>
2017-12-22 21:20:51 +0800

    complete the E reg module

---
+++ myY-86.srcs/sources_1/new/EReg.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+`include "const_defination.v"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2017/12/22 21:07:30
+// Design Name: 
+// Module Name: EReg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module EReg(
+    input wire rst,
+    input wire clk,
+    input wire[`icodeBus] icode,
+    input wire[`ifunBus] ifun,
+    input wire[`digitsBus] valC,valA,valB,    //valA 和valB从decode.v module 中传来
+    input wire[`regBus] dstE,dstM,srcA,srcB,
+    output reg[`icodeBus] E_icode,
+    output reg[`ifunBus] E_ifun,
+    output reg[`digitsBus] E_valC,E_valA,E_valB,
+    output reg[`regBus] E_dstE,E_dstM,E_srcA,E_srcB
+    );
+    
+    always@(posedge clk)
+    begin
+        if(rst==1)
+        begin
+        end
+        else
+        begin
+            E_icode<=icode;
+            E_ifun<=ifun;
+            E_valC<=valC;
+            E_valA<=valA;
+            E_valB<=valB;
+            E_dstE<=dstE;
+            E_dstM<=dstM;
+            E_srcA<=srcA;
+            E_srcB<=srcB;
+        end
+    end
+    
+endmodule
